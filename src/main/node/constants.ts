f856538a3e44a1605308a3cb1bfec7223947f326
Liju Kuriakose <lijukrks166@gmail.com>
2022-05-05 11:23:41 +0530

    feat: added git.service method to clone a repo

--- src/main/node/constants.ts
+++ src/main/node/constants.ts
@@ -3,4 +3,8 @@ export const constants = {
     DEFAULT_NODE_ENV: 'dev',
     NODE_PROD_ENV: 'prod',
     LOGGER_TIME_STAMP_FORMAT: 'YYYY-MM-DD HH:mm:ss',
+    CONTENT_DISPOSITION: 'content-disposition',
+    FILENAME: 'filename',
+    TEMP_DIR: './tmp',
+    UTF_8: 'utf-8',
 } as const;
--- src/main/node/service/git.service.ts
+++ src/main/node/service/git.service.ts
@@ -1,6 +1,10 @@
 import { log } from '@config';
+import { constants } from '@constants';
 import { properties } from '@properties';
 import axios, { AxiosRequestConfig } from 'axios';
+import { existsSync } from 'fs';
+import { mkdir, writeFile } from 'fs/promises';
+import { resolve } from 'path';
 
 export const getUser = async (token: string) => {
     log.info('getting github user info');
@@ -29,6 +33,40 @@ export const getRepoWebHooks = async (token: string, user: string, repo: string)
         url: `/repos/${user}/${repo}/hooks`
     }).then((res) => res.data || []);
 };
+
+export const getRepositoryContents = async (token: string, user: string, repo: string, path: string = '') => {
+    log.info('getting contents of the repo %s at path %s', repo, path);
+    return request<any[]>(token, {
+        url: `/repos/${user}/${repo}/contents/${path}`
+    }).then((res) => res.data || []);
+}
+
+export const cloneRepository = async (token: string, user: string, repo: string) => {
+    log.info('cloning repository %s of user %s', repo, user);
+    return request<any>(token, {
+        url: `/repos/${user}/${repo}/tarball`,
+        responseType: 'blob',
+    }).then(async (response) => {
+        const fileName = (
+            response.headers[constants.CONTENT_DISPOSITION]
+                .split(';')
+                .map((part) => part.trim())
+                .find((part) => part.startsWith(constants.FILENAME)) as string
+        ).split("=")[1];
+        log.debug('saving %s to tmp directory %s', fileName, constants.TEMP_DIR);
+        const path = resolve(constants.TEMP_DIR, fileName);
+        if (!existsSync(constants.TEMP_DIR)) {
+            log.verbose('making temp dir %s', constants.TEMP_DIR);
+            await mkdir(constants.TEMP_DIR);
+        }
+        await writeFile(path, response.data, {
+            encoding: constants.UTF_8,
+        });
+        log.debug('file %s saved', path);
+        return path;
+    });
+};
+
 
 
 const request = <T>(token: string, config: AxiosRequestConfig) => axios.request<T>({
