c32cb11f0d2916c8b68f1a2520ec9af919dcff50
Liju Kuriakose <lijukrks166@gmail.com>
2022-05-05 17:10:51 +0530

    feat: added schema and resolver for getting repo file content

--- src/main/node/routes/index.route.ts
+++ src/main/node/routes/index.route.ts
@@ -5,6 +5,7 @@ import { Router } from 'express';
 import { graphqlHTTP, OptionsData } from 'express-graphql';
 import { buildSchema } from 'graphql';
 import httpStatus from 'http-status';
+import { basename, join, sep } from 'path';
 
 export const router = Router();
 
@@ -12,7 +13,7 @@ export const router = Router();
 let schema = buildSchema(`
   type Query {
       repositories(token: String!): [RepositoryShort!]!
-      repository(token: String!, name: String!): Repository!
+      repository(token: String!, name: String!, noCache: Boolean = false): Repository!
   }
 
   type RepositoryShort {
@@ -27,7 +28,14 @@ let schema = buildSchema(`
       owner: String!
       private: Boolean!
       numberOfFiles: Int!
+      files(pattern: String = "^.*yml$", random: Boolean = false): [File!]!
       activeWebHooks: [WebHook!]!
+  }
+
+  type File {
+      name: String!
+      url: String!
+      contentBase64: String!
   }
 
   type WebHook {
@@ -48,10 +56,10 @@ var root = {
                 })
             ));
     },
-    repository: async ({ token, name }: any) => {
+    repository: async ({ token, name, noCache }: any) => {
         const user = await githubService.getUser(token);
         let repository: any;
-        let repositoryFiles: any[];
+        let repositoryFiles: string[];
         return {
             name: async () => {
                 if (!repository) {
@@ -86,12 +94,35 @@ var root = {
                     }));
             },
             numberOfFiles: async () => {
+                const path = await githubService.cloneRepository(token, user.login, name, noCache as boolean);
                 if (!repositoryFiles) {
-                    const path = await githubService.cloneRepository(token, user.login, name);
                     repositoryFiles = await AppUtils.listArchiveFiles(path);
                 }
                 return repositoryFiles.length;
             },
+            files: async ({ pattern, random }: any) => {
+                const path = await githubService.cloneRepository(token, user.login, name, noCache as boolean);
+                if (!repositoryFiles) {
+                    repositoryFiles = await AppUtils.listArchiveFiles(path);
+                }
+                const reg = new RegExp(pattern);
+                let match = repositoryFiles.filter((file) => reg.test(file));
+                if(random) {
+                    match = [match[Math.ceil(Math.random() * match.length)]];
+                }
+                console.log(match);
+                return match.map(async (file) => {
+                    const parts = file.split(sep);
+                    parts.shift();
+                    const filePath = join(...parts);
+                    const repositoryFile = (await githubService.getRepositoryContents(token, user.login, name, filePath)) as any;
+                    return {
+                        name: filePath,
+                        url: repositoryFile.download_url,
+                        contentBase64: repositoryFile.content
+                    };
+                });
+            }
         }
     },
 };
